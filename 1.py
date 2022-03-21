15e791efb57518b9128eb27c9e7a05508e268ea3
Fedor Ptitsyn <44300071+fptitsyn@users.noreply.github.com>
2022-03-21 20:56:36 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+import turtle
+
+if __name__ == "__main__":
+    turtle.shape("turtle")
+    for _ in range(5):
+        for _ in range(2):
+            turtle.forward(40)
+            turtle.right(60)
+            turtle.forward(40)
+            turtle.right(120)
+        turtle.right(90)
+    turtle.exitonclick()
+    
